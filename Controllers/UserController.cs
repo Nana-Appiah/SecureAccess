e3dd0684e31683402b56b17b5ecce474deb92d41
Nana-Appiah <nappiah@panafricansl.com>
2022-03-27 12:43:11 +0100

    Integration of PortSight SecureAccess Membership code

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -16,104 +16,174 @@ using System.Configuration;
 using SecureAuth.Models;
 using System.Web.Http.Cors;
 using System.Web.Cors;
+using System.Data;
+using System.Diagnostics;
 
 namespace SecureAuth.Controllers
 {
     [EnableCors(origins: "https://localhost:7111", headers:"*",methods:"*")]
     public class UserController : ApiController
     {
-       
-        [HttpPost]
-        public async Task<bool> User(string[] model)
+
+        private ARConnection conn;
+        public UserController()
         {
-            var str = ConfigurationManager.AppSettings["connstring"].ToString();
-            var conn = new ARConnection(str);
-            conn.ConnectToCatalog();
-
-            var o = ARHelper.AuthenticateUser(model[0],model[1],str, Functions.GetAES256EncryptionKey(),
-                                               Functions.GetAES256InitializationVector(),Functions.GetAES256CipherStringFormat());
-            if(o == ARAuthenticationResultsEnum.OK)
-            {
-                var user = new ARUser();
-                user = conn.GetUserByLogin(model[0]);
-
-                var u = new ARUserTicket(user);
-
-                return true;
-            }
-            else { return false; }
+            
         }
 
-        [HttpGet]
-        public bool isAuthorized(string[] data)
+        private void InitializeConnectionObject()
         {
-            //username
-            //resource alias (etas.admin or etas.student)
-            //permission list (create , read,update, delete)
+            var str = ConfigurationManager.AppSettings["connstring"].ToString();
+            conn = new ARConnection(str);
+            conn.ConnectToCatalog();  //ARCon object
+        }
+
+        [HttpPost]
+        public bool User(string[] model)
+        {
             try
             {
-                var str = ConfigurationManager.AppSettings["connstring"].ToString();
-                var conn = new ARConnection(str);
-                conn.ConnectToCatalog();
+                InitializeConnectionObject();
+                var o = ARHelper.AuthenticateUser(model[0], model[1], conn.ConnectionString, Functions.GetAES256EncryptionKey(),
+                                                   Functions.GetAES256InitializationVector(), Functions.GetAES256CipherStringFormat());
+                if (o == ARAuthenticationResultsEnum.OK)
+                {
+                    var user = new ARUser();
+                    user = conn.GetUserByLogin(model[0]);
 
-                var user = new ARUser();
-                user = conn.GetUserByLogin(data[0]);
+                    var u = new ARUserTicket(user);
 
-                var u = new ARUserTicket(user);
-                var membership = u.MembershipObjectAliases.Split(';');
-
-                bool bln = Array.Exists(membership, x => x == data[1].ToString());
-
-                //bool bln = ARHelper.IsAuthorized(data[0], data[1], data[2]);
-
-                return bln;
+                    //calling isAuthorized from here
+                    var bln = IsAuthorized(u, model[1], model[2], conn);
+                    return bln;
+                }
+                else { return false; }
             }
-            catch (Exception x)
+            catch(Exception ee)
             {
+                Debug.Print($"error: {ee.Message}");
                 return false;
             }
         }
 
-        //private bool IsAuthorized(ARUserTicket ticket, string resourcealias, string permissionalias)
+        //[HttpGet]
+        //public bool isAuthorized(string[] data)
         //{
-        //    var str = ConfigurationManager.AppSettings["connstring"].ToString();
-        //    var ARConn = new ARConnection(str);
-        //    ARConn.ConnectToCatalog();
-
-        //    if (ARHelper.IsAuthorized(ticket.Login, resourcealias, permissionalias))
-        //        return true;
-        //    else
+        //    //username
+        //    //resource alias (etas.admin or etas.student)
+        //    //permission list (create , read,update, delete)
+        //    try
         //    {
-        //        // gets all operators authorized for this permission
-        //        ARObject authorizedOperator;
-        //        foreach (var authorizedOperator in GetAuthorisedOperators(ARConn.GetResourceByAlias(resourcealias).ObjectID))
-        //        {
-        //            foreach (string membership in ticket.MembershipObjectAliases.Split(";"))
-        //            {
-        //                if (!membership == "")
-        //                {
-        //                    AROperator arOperator;
-        //                    if (!ARConn.GetGroupByAlias(membership) == null)
-        //                    {
-        //                        if (authorizedOperator.ObjectID == ARConn.GetGroupByAlias(membership).ObjectID)
-        //                        {
-        //                            arOperator = ARConn.GetOperatorByID(authorizedOperator.ObjectID);
-        //                            return arOperator.IsAuthorized(resourcealias, permissionalias);
-        //                        }
-        //                    }
-        //                    else if (!ARConn.GetRoleByAlias(membership) == null)
-        //                    {
-        //                        if (authorizedOperator.ObjectID == ARConn.GetRoleByAlias(membership).ObjectID)
-        //                        {
-        //                            arOperator = ARConn.GetOperatorByID(authorizedOperator.ObjectID);
-        //                            return arOperator.IsAuthorized(resourcealias, permissionalias);
-        //                        }
-        //                    }
-        //                }
-        //            }
-        //        }
+        //        var str = ConfigurationManager.AppSettings["connstring"].ToString();
+        //        var conn = new ARConnection(str);
+        //        conn.ConnectToCatalog();
+
+        //        var user = new ARUser();
+        //        user = conn.GetUserByLogin(data[0]);
+
+        //        var u = new ARUserTicket(user);
+        //        var membership = u.MembershipObjectAliases.Split(';');
+
+        //        bool bln = Array.Exists(membership, x => x == data[1].ToString());
+
+        //        //bool bln = ARHelper.IsAuthorized(data[0], data[1], data[2]);
+
+        //        return bln;
+        //    }
+        //    catch (Exception x)
+        //    {
+        //        return false;
         //    }
         //}
+
+        [HttpGet]
+        public bool IsAuthorized(ARUserTicket ticket, string resourcealias, string permissionalias, ARConnection ARConn)
+        {
+            if (ARHelper.IsAuthorized(ticket.Login, resourcealias, permissionalias))
+            {
+                return true;
+            }
+            else
+            {
+                // gets all operators authorized for this permission
+                bool returnValue = false;
+
+                foreach (ARObject authorizedOperator in GetAuthorisedOperators(ARConn.GetResourceByAlias(resourcealias).ObjectID))
+                {
+                    AROperator arOperator;
+                    foreach (string membership in ticket.MembershipObjectAliases.Split(';'))
+                    {
+                        if (membership != "")
+                        {
+                            if (ARConn.GetGroupByAlias(membership) != null)
+                            {
+                                if (authorizedOperator.ObjectID == ARConn.GetGroupByAlias(membership).ObjectID)
+                                {
+                                    arOperator = ARConn.GetOperatorByID(authorizedOperator.ObjectID);
+                                    returnValue = arOperator.IsAuthorized(resourcealias, permissionalias);
+                                    return returnValue;
+                                }
+                                else 
+                                { 
+                                    return false; 
+                                }
+                            }
+                            else
+                            { 
+                                return false; 
+                            }
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                return returnValue;
+            }
+        }
+
+
+        private ARObjectsCollection GetAuthorisedOperators(int ResourceID)
+        {
+            ARObjectsCollection o = new ARObjectsCollection();
+
+            DataRow dr = null; // TODO Change to default(_) if this is not a reference type /;
+            DataSet ds;
+            ARDBObject ardbobj = new ARDBObject();
+            ARObject arobj;
+
+            ds = ardbobj.SelectPermissionMatrix(ResourceID);
+
+            string lastValue = "";
+            int rowIndex = 0;
+            int period = 0;
+
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+
+                lastValue = ds.Tables[0].Rows[0]["ChildObjectID"].ToString();
+                // find the row-repeating period
+                while (lastValue == ds.Tables[0].Rows[rowIndex]["ChildObjectID"].ToString())
+                {
+                    rowIndex += 1;
+                    if (rowIndex == ds.Tables[0].Rows.Count)
+                        break;
+                }
+
+                period = rowIndex;
+
+                for (rowIndex = 0; rowIndex <= ds.Tables[0].Rows.Count - 1; rowIndex += period)
+                {
+                    arobj = conn.GetObjectByID(int.Parse(ds.Tables[0].Rows[rowIndex]["ChildObjectID"].ToString()));
+                    o.Add(arobj);
+                }
+            }
+
+            return o;
+        }
+
 
     }
 }
